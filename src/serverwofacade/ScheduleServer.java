e54a512b0c2e412f2567c822f6ac419f603b2953
21 September <Thammanit.ch@gmail.com>
2022-09-02 09:22:54 +0700

    Add facade server

--- src/serverwofacade/ScheduleServer.java
+++ src/serverwofacade/ScheduleServer.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package serverwofacade;
+// package serverwofacade;
 
 /**
  *
--- src/serverwofacade/ServerWOFacade.java
+++ src/serverwofacade/ServerWOFacade.java
@@ -4,24 +4,11 @@
  * and open the template in the editor.
  */
 
-package serverwofacade;
+// package serverwofacade;
 public class ServerWOFacade {
     public static void main(String[] args) {
-        ScheduleServer scheduleServer = new ScheduleServer();
-	scheduleServer.startBooting();
-	scheduleServer.readSystemConfigFile();
-	scheduleServer.init();
-	scheduleServer.initializeContext();
-	scheduleServer.initializeListeners();
-	scheduleServer.createSystemObjects();
-	System.out.println("Start working......");
-	System.out.println("After work done.........");
-	scheduleServer.releaseProcesses();
-	scheduleServer.destory();
-	scheduleServer.destroySystemObjects();
-	scheduleServer.destoryListeners();
-	scheduleServer.destoryContext();
-	scheduleServer.shutdown();
-    }
-    
+        FacadeServer server = new FacadeServer();
+		server.startServer();
+		server.stopServer();
+    }   
 }
